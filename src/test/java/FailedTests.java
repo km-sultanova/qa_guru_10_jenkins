aefa762242c4c38d83b74aa7cc84d28974ac0357
Karina Sultanova <k.sultanova@dodopizza.com>
2022-05-25 00:03:32 +0600

    init commit

---
+++ src/test/java/FailedTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FailedTests {
+    @Test
+    void test00(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test01(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test02(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test03(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test04(){
+        assertTrue(false);
+    }
+
+}
